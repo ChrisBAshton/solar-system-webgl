ee71ea397bd6d78105e95adda9c2c45ba7f7bd1a
ChrisBAshton <chrisashtonweb@gmail.com>
2014-10-24 11:40:33 +0100

    getting closer to working orbits

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 define(['glMatrix', 'glUtils', 'astronomical_object', 'gl', 'shaders'], function (glMatrix, glUtils, AstronomicalObject, gl, shaders) {
 
     var shaderProgram,
-        projectionMatrix;
+        cameraMatrix,
+        projectionMatrix,
+        projectionViewMatrix;
 
     function init() {
         var canvas = document.getElementById('canvas_solar_system');
         initViewport(gl, canvas);
 
         var theSun = new AstronomicalObject({
-            origin:  [0, -2, -12.333],
-            orbits:  false,
-            radius:  300,
+            name:    "Sun",
+            origin:  [0, 0, 0],
+            radius:  1000,
             axis:    0,
-            texture: 'http://www.corsproxy.com/learningwebgl.com/lessons/lesson11/moon.gif',
+            texture: 'http://www.corsproxy.com/learningwebgl.com/lessons/lesson11/moon.gif'/*,
             faceColors: [
                 [1.0, 1.0, 1.0, 1.0], // Front face
                 [1.0, 1.0, 1.0, 1.0], // Back face
@@ -20,25 +22,37 @@ define(['glMatrix', 'glUtils', 'astronomical_object', 'gl', 'shaders'], function
                 [1.0, 1.0, 0.0, 1.0], // Bottom face
                 [1.0, 1.0, 1.0, 1.0], // Right face
                 [1.0, 1.0, 1.0, 1.0]  // Left face
-            ]
+            ]*/
         });
+
+        var mercury = new AstronomicalObject({
+            name:          "Mercury",
+            orbits:        theSun,
+            orbitDistance: 1000,
+            radius:        100,
+            axis:          0,
+            texture:       'http://www.corsproxy.com/learningwebgl.com/lessons/lesson11/moon.gif'
+        });
+
         var earth = new AstronomicalObject({
-            origin:  [0, 2, -30.333],
-            orbits:  theSun,
-            radius:  100,
-            axis:    0,
-            texture: 'http://www.corsproxy.com/learningwebgl.com/lessons/lesson11/moon.gif'
+            name:          "Earth",
+            orbits:        theSun,
+            orbitDistance: 2200,
+            radius:        300,
+            axis:          0,
+            texture:       'http://www.corsproxy.com/learningwebgl.com/lessons/lesson11/moon.gif'
         });
 
         var moon = new AstronomicalObject({
-            origin:  [0, 2, -50.333],
-            orbits:  earth,
-            radius:  50,
-            axis:    0,
-            texture: 'http://www.corsproxy.com/learningwebgl.com/lessons/lesson11/moon.gif'
+            name:          "Moon",
+            orbits:        earth,
+            orbitDistance: 150,
+            radius:        90,
+            axis:          0,
+            texture:       'http://www.corsproxy.com/learningwebgl.com/lessons/lesson11/moon.gif'
         });
 
-        var solarSystem = [theSun, earth, moon];
+        var solarSystem = [theSun, mercury, earth, moon];
 
         initMatrices(canvas);
         shaderProgram = shaders.init();
@@ -51,6 +65,18 @@ define(['glMatrix', 'glUtils', 'astronomical_object', 'gl', 'shaders'], function
     }
 
     function initMatrices(canvas) {
+
+        projectionViewMatrix = glMatrix.mat4.create();
+
+        cameraMatrix = glMatrix.mat4.create();
+
+        glMatrix.mat4.lookAt(
+            cameraMatrix, // the matrix we're writing to
+            [0, 10000, 10000], // position of the camera in World Space
+            [0, 0, 0], // where we're looking at in World Space
+            [0, 1, 0] // [0, -1, 0] would draw the world upside down
+        );
+
         // Create a project matrix with 45 degree field of view
         projectionMatrix = glMatrix.mat4.create();
         glMatrix.mat4.perspective(
@@ -58,18 +84,25 @@ define(['glMatrix', 'glUtils', 'astronomical_object', 'gl', 'shaders'], function
             Math.PI / 4,        // 45 degree field of view
             canvas.width / canvas.height,
             1,
-            10000
+            100000
         );
+
+        glMatrix.mat4.multiply(projectionViewMatrix, projectionMatrix, cameraMatrix);
     }
 
-    function run(gl, solarSystem) {
+    function run(gl, solarSystem, timesRan) {
 
-        var test = false;
+        var test = true,
+            numberOfFramesToRun = 3000;
+
+        timesRan = timesRan || 1;
 
         if (test) {
-            setTimeout(function () {
-                run(gl, solarSystem);
-            }, 1000);
+            if (timesRan < numberOfFramesToRun) {
+                setTimeout(function () {
+                    run(gl, solarSystem, ++timesRan);
+                }, 100);
+            }
         } else {
             requestAnimationFrame(function() {
                 run(gl, solarSystem);
@@ -91,7 +124,7 @@ define(['glMatrix', 'glUtils', 'astronomical_object', 'gl', 'shaders'], function
 
         for (var i = 0; i < solarSystem.length; i++) {
             var planet = solarSystem[i];
-            shaders.getReadyToDraw(projectionMatrix, planet.getModelViewMatrix(), planet);
+            shaders.getReadyToDraw(projectionViewMatrix, planet.getModelViewMatrix(), planet);
             planet.draw();
         }
     }
--- js/astronomical_object.js
+++ js/astronomical_object.js
@@ -2,39 +2,60 @@ define(['gl', 'glMatrix'], function (gl, glMatrix) {
 
     var AstronomicalObject = function (config) {
 
-        this.origin        = config.origin        || [0, 0, -4];
-        this.orbits        = config.orbits        || false;
-        this.orbitDistance = config.orbitDistance || 0;
-        this.radius        = config.radius        || 10;
-        this.axis          = config.axis          || 0;
-        this.texture       = config.texture       || false;
-        this.faceColors    = config.faceColors || [
-            [1.0, 0.0, 0.0, 1.0], // Front face
-            [0.0, 1.0, 0.0, 1.0], // Back face
-            [0.0, 0.0, 1.0, 1.0], // Top face
-            [1.0, 1.0, 0.0, 1.0], // Bottom face
-            [1.0, 0.0, 1.0, 1.0], // Right face
-            [0.0, 1.0, 1.0, 1.0]  // Left face
-        ];
+        this.setAttributes(config);
+        this.setOriginAccordingTo(config);
 
         this.modelViewMatrix = glMatrix.mat4.create();
         glMatrix.mat4.translate(this.modelViewMatrix, this.modelViewMatrix, this.origin);
+        glMatrix.mat4.translate(this.modelViewMatrix, this.modelViewMatrix, [
+            0, 0, -(this.radius / 2)
+        ]);
 
-        this.createCube();
+        this.createCube(this.radius);
     };
 
     AstronomicalObject.prototype = {
 
+        setAttributes: function (config) {
+            this.name          = config.name          || 'name not set';
+            this.orbits        = config.orbits        || false;
+            this.orbitDistance = config.orbitDistance || 0;
+            this.radius        = config.radius        || 10;
+            this.axis          = config.axis          || 0;
+            this.texture       = config.texture       || false;
+            this.faceColors    = config.faceColors    || [
+                [1.0, 0.0, 0.0, 1.0], // Front face
+                [0.0, 1.0, 0.0, 1.0], // Back face
+                [0.0, 0.0, 1.0, 1.0], // Top face
+                [1.0, 1.0, 0.0, 1.0], // Bottom face
+                [1.0, 0.0, 1.0, 1.0], // Right face
+                [0.0, 1.0, 1.0, 1.0]  // Left face
+            ];
+        },
+
+        setOriginAccordingTo: function (config) {
+
+            console.log("Setting origin for the " + this.name);
+
+            if (this.orbits) {
+                this.origin = this.orbits.origin;
+                this.origin[2] -= (this.orbitDistance + this.orbits.radius + this.radius);
+            }
+            else {
+                this.origin = config.origin || [0, 0, 0];
+            }
+
+            console.log('origin: ', this.origin);
+        },
+
         // Create the vertex, color and index data for a multi-colored cube
-        createCube: function () {
+        createCube: function (scaleFactor) {
 
             // Vertex Data
             var vertexBuffer;
             vertexBuffer = gl.createBuffer();
             gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
             
-            var scaleFactor = this.radius / 300;
-
             var verts = [
                // Front face
                -1.0 * scaleFactor, -1.0 * scaleFactor,  1.0 * scaleFactor,
@@ -124,38 +145,33 @@ define(['gl', 'glMatrix'], function (gl, glMatrix) {
         currentTime: Date.now(),
 
         spin: function () {
-            var now = Date.now();
-            var deltat = now - this.currentTime;
-            this.currentTime = now;
-            var fract = deltat / this.spinDuration;
-            var angle = Math.PI * 2 * fract;
-            glMatrix.mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, [0, 1, 1]);
+            // var now = Date.now();
+            // var deltat = now - this.currentTime;
+            // this.currentTime = now;
+            // var fract = deltat / this.spinDuration;
+            // var angle = Math.PI * 2 * fract; // @TODO use this.axis somewhere
+            // glMatrix.mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, [0, 1, 1]);
         },
 
         orbit: function () {
             if (this.orbits) {
-                
-                var matrixOfCurrentPlanet       = this.modelViewMatrix;
-                var matrixOfPlanetWeAreOrbiting = this.orbits.modelViewMatrix;
+                // the temporary matrix we'll use to make our planet orbit
+                var translationMatrix = glMatrix.mat4.create();
 
-                var orbitMatrix = glMatrix.mat4.create();
-                var inverseMatrixOfPlanetWeAreOrbiting = glMatrix.mat4.create();
+                // matrix of planet we're orbiting
+                var parentMatrix = this.orbits.modelViewMatrix;
 
-                // start with clean identity matrix
-                glMatrix.mat4.identity(orbitMatrix);
-                glMatrix.mat4.identity(orbitMatrix);
-
-                // move to matrix of planet we're orbiting
-                glMatrix.mat4.multiply(orbitMatrix, orbitMatrix, matrixOfPlanetWeAreOrbiting);
-
-                // rorate
-                glMatrix.mat4.rotate(orbitMatrix, orbitMatrix, 0.1, [1, 0, 0]);
+                // start off at parent planet's matrix
+                glMatrix.mat4.copy(translationMatrix, parentMatrix);
+                // rotate
+                glMatrix.mat4.rotate(translationMatrix, translationMatrix, 0.1, [0, 1, 0]);
 
                 // move back by same distance, but at different angle (hence orbiting)
-                glMatrix.mat4.invert(inverseMatrixOfPlanetWeAreOrbiting, matrixOfPlanetWeAreOrbiting);
-                glMatrix.mat4.multiply(orbitMatrix, orbitMatrix, inverseMatrixOfPlanetWeAreOrbiting);
-
-                glMatrix.mat4.multiply(this.modelViewMatrix, this.modelViewMatrix, orbitMatrix);
+                var inverseParentMatrix = glMatrix.mat4.create();
+                glMatrix.mat4.invert(inverseParentMatrix, parentMatrix);
+                glMatrix.mat4.multiply(translationMatrix, translationMatrix, inverseParentMatrix);
+                
+                glMatrix.mat4.multiply(this.modelViewMatrix, this.modelViewMatrix, translationMatrix);
             }
         }
 
