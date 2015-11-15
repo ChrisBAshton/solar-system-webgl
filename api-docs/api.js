d8f54bea3f9b97c848accafa781860a720f0398d
ChrisBAshton <chrisashtonweb@gmail.com>
2015-11-15 09:29:38 +0000

    published api docs

---
+++ api-docs/api.js
@@ -0,0 +1,70 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "App",
+        "AstronomicalObject",
+        "Buffers",
+        "Camera",
+        "Controls",
+        "ControlsGUI",
+        "GL",
+        "Lighting",
+        "Shaders"
+    ],
+    "modules": [
+        "App",
+        "AstronomicalObject",
+        "Buffers",
+        "Camera",
+        "Controls",
+        "ControlsGUI",
+        "GL",
+        "Lighting",
+        "Shaders",
+        "SolarSystem"
+    ],
+    "allModules": [
+        {
+            "displayName": "App",
+            "name": "App",
+            "description": "Initialises the application"
+        },
+        {
+            "displayName": "AstronomicalObject",
+            "name": "AstronomicalObject"
+        },
+        {
+            "displayName": "Buffers",
+            "name": "Buffers"
+        },
+        {
+            "displayName": "Camera",
+            "name": "Camera"
+        },
+        {
+            "displayName": "Controls",
+            "name": "Controls"
+        },
+        {
+            "displayName": "ControlsGUI",
+            "name": "ControlsGUI"
+        },
+        {
+            "displayName": "GL",
+            "name": "GL"
+        },
+        {
+            "displayName": "Lighting",
+            "name": "Lighting"
+        },
+        {
+            "displayName": "Shaders",
+            "name": "Shaders"
+        },
+        {
+            "displayName": "SolarSystem",
+            "name": "SolarSystem"
+        }
+    ]
+} };
+});
